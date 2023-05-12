10b835f20fd30e9c4ee1dc07f20cd85355daaaa6
ssatriya <ssatriya@gmail.com>
2023-05-12 14:01:02 +0800

    Refactor code for accessibility

--- components/ContentSwitch.tsx
+++ components/ContentSwitch.tsx
@@ -7,12 +7,8 @@ const ContentSwitch = ({ contentType, switchHandler }: any) => {
   return (
     <Tabs defaultValue={contentType} onValueChange={switchHandler}>
       <TabsList>
-        <TabsTrigger value="surat" aria-label="surat">
-          Surat
-        </TabsTrigger>
-        <TabsTrigger value="juz" aria-label="juz">
-          Juz
-        </TabsTrigger>
+        <TabsTrigger value="surat">Surat</TabsTrigger>
+        <TabsTrigger value="juz">Juz</TabsTrigger>
       </TabsList>
     </Tabs>
   );
--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -11,7 +11,9 @@ import { AppDispatch, RootState } from "@/store/store";
 import { fetchJuz, fetchSurat, setCurrentType } from "@/store/content-slice";
 import Surat from "./Surat";
 import { buttonVariants } from "./ui/button";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 import Loading from "@/app/loading";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 const MainContent = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -55,49 +57,59 @@ const MainContent = () => {
 
   return (
     <main>
-      <div className="mb-4">
-        <ContentSwitch
+      {/* <div className="mb-4"> */}
+      {/* <ContentSwitch
           contentType={contentType}
           switchHandler={switchHandler}
-        />
-      </div>
-      {contentLoading && <Loading />}
-      <div className="grid desktop:grid-cols-3 tablet:grid-cols-2 mobile:grid-cols-1 gap-3">
-        {contentType === "surat" &&
-          surat?.chapters.map((surat: SuratsType) => {
-            return <Surat key={surat.id} surat={surat} />;
-          })}
-      </div>
-      {contentType === "juz" &&
-        juz?.juzs.map((juz) => {
-          return (
-            <div
-              key={juz.id}
-              className="p-3 border border-slate-200 dark:border-accent rounded-md my-2 w-full"
-            >
-              <div className="flex justify-between items-center">
-                <Link
-                  href={`/juz/${juz.id}`}
-                  className={buttonVariants({ variant: "ghost" })}
+        /> */}
+      <Tabs defaultValue={contentType} onValueChange={switchHandler}>
+        <TabsList className="mb-4">
+          <TabsTrigger value="surat">Surat</TabsTrigger>
+          <TabsTrigger value="juz">Juz</TabsTrigger>
+        </TabsList>
+        {/* </div> */}
+        <TabsContent value="surat">
+          {contentLoading && <Loading />}
+          <div className="grid desktop:grid-cols-3 tablet:grid-cols-2 mobile:grid-cols-1 gap-3">
+            {contentType === "surat" &&
+              surat?.chapters.map((surat: SuratsType) => {
+                return <Surat key={surat.id} surat={surat} />;
+              })}
+          </div>
+        </TabsContent>
+        <TabsContent value="juz">
+          {contentType === "juz" &&
+            juz?.juzs.map((juz) => {
+              return (
+                <div
+                  key={juz.id}
+                  className="p-3 border border-slate-200 dark:border-accent rounded-md my-2 w-full"
                 >
-                  Jus {juz.juz_number}
-                </Link>
-                <Link
-                  href={`/juz/${juz.id}`}
-                  className={buttonVariants({ variant: "outline" })}
-                >
-                  Baca Juz
-                </Link>
-              </div>
-              <div className="mt-4">
-                {Object.keys(juz.verse_mapping).map((verse, index) => {
-                  const suratFromJuz = surat?.chapters[parseInt(verse) - 1];
-                  return <Surat key={index} surat={suratFromJuz} />;
-                })}
-              </div>
-            </div>
-          );
-        })}
+                  <div className="flex justify-between items-center">
+                    <Link
+                      href={`/juz/${juz.id}`}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Jus {juz.juz_number}
+                    </Link>
+                    <Link
+                      href={`/juz/${juz.id}`}
+                      className={buttonVariants({ variant: "outline" })}
+                    >
+                      Baca Juz
+                    </Link>
+                  </div>
+                  <div className="mt-4">
+                    {Object.keys(juz.verse_mapping).map((verse, index) => {
+                      const suratFromJuz = surat?.chapters[parseInt(verse) - 1];
+                      return <Surat key={index} surat={suratFromJuz} />;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+        </TabsContent>
+      </Tabs>
     </main>
   );
 };
