64298c0d6edac76e9c7f46d958bbc799f47a4202
ssatriya <ssatriya@gmail.com>
2023-05-12 13:42:10 +0800

    Accessibility fix

--- components/ContentSwitch.tsx
+++ components/ContentSwitch.tsx
@@ -5,16 +5,12 @@ import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 
 const ContentSwitch = ({ contentType, switchHandler }: any) => {
   return (
-    <Tabs
-      defaultValue={contentType}
-      onValueChange={switchHandler}
-      aria-labelledby="quran content switch"
-    >
+    <Tabs defaultValue={contentType} onValueChange={switchHandler}>
       <TabsList>
-        <TabsTrigger value="surat" aria-labelledby="surat">
+        <TabsTrigger value="surat" aria-labelledby="surat" aria-label="surat">
           Surat
         </TabsTrigger>
-        <TabsTrigger value="juz" aria-labelledby="juz">
+        <TabsTrigger value="juz" aria-labelledby="juz" aria-label="juz">
           Juz
         </TabsTrigger>
       </TabsList>
